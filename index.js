82364d0e91e125b420873952f52efb9438a1944a
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-04-05 16:29:23 +0700

    attempt migration 2

---
+++ index.js
@@ -0,0 +1,18 @@
+if (window.top == window)
+    window.location.href = "//ducdat0507.github.io";
+
+window.addEventListener("message", (e) => {
+    console.log(e);
+    if (e.origin != "https://ducdat0507.github.io") return;
+    let source = e.source;
+    let data = e.data;
+    let type = e.data?.type;
+    if (type == "duducat-set-local-storage") {
+        localStorage.setItem(data.key, data.value);
+        source.postMessage({
+            type: "duducat-set-local-storage",
+            key: data.key,
+            success: true,
+        })
+    }
+});
